fc0bcb57edb5150928e8819e129b8e45b6929650
Michel Azevedo <michel.azevedos@gmail.com>
2021-06-04 02:38:39 -0300

    Complete flow

--- lib/status_check.ex
+++ lib/status_check.ex
@@ -1,9 +1,14 @@
 defmodule StatusCheck do
+  alias StatusCheck.StatusChecker
+
   @moduledoc """
   retrieves status code and response time from a list of urls
   """
 
-  def main(_urls \\ []) do
-    IO.puts "Hi 👋"
+  def main(urls \\ []) do
+    urls
+    |> Enum.map(&Task.async(fn -> StatusChecker.get(&1) end))
+    |> Enum.map(&Task.await/1)
+    |> Enum.each(&IO.puts/1)
   end
 end
